639ce121a7d890824e86a98e2a4addac9b4deb20
Equinoz <julien.lecoz33@gmail.com>
2021-08-11 10:47:18 +0200

    Desactivate removed users' tokens

--- app/middlewares/auth.js
+++ app/middlewares/auth.js
@@ -4,7 +4,7 @@
  */
 
 const jwt = require("jsonwebtoken"),
-      { Token } = require("../models");
+      { User, Token } = require("../models");
 
 const auth = async (req, res, next) => {
   const token = req.headers.authorization;
@@ -17,12 +17,18 @@ const auth = async (req, res, next) => {
 
     // If the token is valid id's user and status' user saved in the locals variables scoped to the request
     const decodedToken = jwt.verify(token, process.env.SECRET_TOKEN);
+    if (! await User.findById(decodedToken.userId)) {
+      const error = new Error();
+      error.name = "UserDoesNotExistError";
+      throw error;
+    }
+
     res.locals.userId = decodedToken.userId;
     res.locals.userStatus_id = decodedToken.userStatus_id;
 
     next();
   } catch (err) {
-    if (err.name == "JsonWebTokenError" || err.name == "TokenExpiredError" || err.name == "NotBeforeError") {
+    if (err.name == "JsonWebTokenError" || err.name == "TokenExpiredError" || err.name == "NotBeforeError" || err.name == "UserDoesNotExistError") {
       res.status(401).json({ error: err.name });
       return;
     }
